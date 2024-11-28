f873439cc4bfcf77a4c1cf764e060899ace4a089
dlckdgh0414 <ci5976547@gmail.com>
2024-11-28 18:44:28 +0900

    feat : ScissorsPhases

--- Assets/02.Script/LCH/Boss/ScissorsFolder/States/Scissors/ScissorsWaitState.cs
+++ Assets/02.Script/LCH/Boss/ScissorsFolder/States/Scissors/ScissorsWaitState.cs
@@ -21,7 +21,7 @@ public class ScissorsWaitState : EnemyState<BossState>
     private IEnumerator PhaseSelect()
     {
         yield return new WaitForSeconds(0.5F);
-        int Phases = /*Random.Range(1, 4);*/ 1;
+        int Phases = /*Random.Range(1, 4);*/ 2;
 
         switch (Phases)
         {
