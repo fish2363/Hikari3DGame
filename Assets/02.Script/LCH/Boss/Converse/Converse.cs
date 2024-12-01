42e7a42721266b9b7fc456e080ac01175195ca9e
dlckdgh0414 <ci5976547@gmail.com>
2024-12-01 21:46:45 +0900

    feat : ConversPhase2

--- Assets/02.Script/LCH/Boss/Converse/Converse.cs
+++ Assets/02.Script/LCH/Boss/Converse/Converse.cs
@@ -11,6 +11,7 @@ public class Converse : BossBass
         BossStateMachine.AddState(BossState.Chase, new ConverseChaseState(this, BossStateMachine, "Chase"));
         BossStateMachine.AddState(BossState.Wait, new ConversWaitState(this, BossStateMachine, "Wait"));
         BossStateMachine.AddState(BossState.Phase1, new ConversPhase1State(this, BossStateMachine, "Phase1"));
+        BossStateMachine.AddState(BossState.Phase2, new ConversPhase2State(this, BossStateMachine, "Phase2"));
     }
 
     private void Start()
