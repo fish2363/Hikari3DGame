43e20f6327ed75091b81c884c7d3b5e70a5e47ef
dlckdgh0414 <ci5976547@gmail.com>
2024-12-03 00:55:46 +0900

    feat : Boss

---
+++ Assets/02.Script/LCH/Boss/PencilSharpener/States/PencilSharpenerChaseState.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PencilSharpenerChaseState : EnemyState<BossState>
+{
+    private PencilSharpener _pencilSharpener;
+    int timer = 0;
+    public PencilSharpenerChaseState(EnemyAgent enemy, StateMachine<BossState> state, string animHashName) : base(enemy, state, animHashName)
+    {
+        _pencilSharpener = enemy as PencilSharpener;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+         _pencilSharpener.StartCoroutine(ChangeWaitState(timer));
+        _pencilSharpener.IsPhaseEnd = false;
+    }
+
+    private IEnumerator ChangeWaitState(int timer)
+    {
+        yield return new WaitForSeconds(timer);
+        _pencilSharpener.BossStateMachine.ChangeState(BossState.Wait);
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+        _pencilSharpener.targetDir = _pencilSharpener.player.transform.position - _pencilSharpener.transform.position;
+        _pencilSharpener.RigidCompo.velocity = _pencilSharpener.targetDir.normalized * _pencilSharpener.EnemyStat.MoveSpeed;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        timer = 0;
+    }
+}
