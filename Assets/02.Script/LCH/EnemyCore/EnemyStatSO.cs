9729275e805385e3630bdd2760703233c18e0176
rlaals0203 <scientistmin@gmail.com>
2024-12-05 01:06:30 +0900

    refactor : rcCar

--- Assets/02.Script/LCH/EnemyCore/EnemyStatSO.cs
+++ Assets/02.Script/LCH/EnemyCore/EnemyStatSO.cs
@@ -8,12 +8,14 @@ public class EnemyStatSO : ScriptableObject
 	[Header("AttackSetting")]
 	public float AttackRadius;
 	public float WallRadius;
-	public float AttackPoawer;
 	public float KnockbackPower;
 	public float AttackDelay;
 	public float ContactAttackRadius;
+    public int MinAttackDamage;
+    public int MaxAttackDamage;
 
-	[Header("EnemySetting")]
+    [Header("EnemySetting")]
+	public string Name;
 	public float HP;
 	public float MoveSpeed;
 }
