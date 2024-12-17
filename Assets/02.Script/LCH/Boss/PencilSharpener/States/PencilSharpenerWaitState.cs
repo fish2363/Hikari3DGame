8274d78412815da43525b6d102ed794b987e37fe
dlckdgh0414 <ci5976547@gmail.com>
2024-12-17 16:22:24 +0900

    fix : BossAnim

--- Assets/02.Script/LCH/Boss/PencilSharpener/States/PencilSharpenerWaitState.cs
+++ Assets/02.Script/LCH/Boss/PencilSharpener/States/PencilSharpenerWaitState.cs
@@ -23,7 +23,7 @@ public class PencilSharpenerWaitState : EnemyState<BossState>
     private IEnumerator PhaseSelect()
     {
         yield return new WaitForSeconds(0.5F);
-        int Phases = Random.Range(1,3);
+        int Phases = /*Random.Range(1,3)*/3;
 
         switch (Phases)
         {
