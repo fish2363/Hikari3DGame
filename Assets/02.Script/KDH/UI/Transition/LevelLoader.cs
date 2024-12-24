3d4205f527595ec088ecba3d1a4b8932798f3320
kimdonghyun81222 <157779631+kimdonghyun81222@users.noreply.github.com>
2024-12-24 18:02:53 +0900

    feat : UIDesign

--- Assets/02.Script/KDH/UI/Transition/LevelLoader.cs
+++ Assets/02.Script/KDH/UI/Transition/LevelLoader.cs
@@ -28,6 +28,6 @@ public class LevelLoader : MonoBehaviour
 
         yield return new WaitForSeconds(transitionTime);
 
-        SceneManager.LoadScene(levelIndex);
+        SceneManager.LoadScene(levelIndex); ;
     }
 }
